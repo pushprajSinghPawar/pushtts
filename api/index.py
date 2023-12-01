925216708e59c0d374f6b3da4090c672159bcf41
pushpraj <pushprajsinghpawar7@gmail.com>
2023-12-01 15:08:39 +0530

    a
    final

--- api/index.py
+++ api/index.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_from_directory
+from gtts import gTTS
+import os
 
 app = Flask(__name__)
 
+# Serve static files with cache-control headers set to no-cache
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(app.config['STATIC_FOLDER'], filename, cache_timeout=0)
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html', audio_file=None)
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    text = request.form.get('txt', '').strip()
+    if text:
+        var = gTTS(text=text, lang="hi")
+        audio_filename = "eng.mp3"  # Same filename every time
+        audio_file_path = os.path.join("static", audio_filename)
+
+        # Delete the old file if it exists
+        if os.path.exists(audio_file_path):
+            os.remove(audio_file_path)
+
+        var.save(audio_file_path)
+        return render_template('answer.html', audio_file=audio_filename, text=text)
+    return render_template('answer.html', audio_file=None, text='')
+
+if __name__ == "__main__":
+    app = Flask(__name__, static_url_path='/static')
+    app.run(debug=True)
